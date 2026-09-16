2cbbb77ab1e75709145bb6d023837984aa2583a0
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-09-17 03:07:00 +0530

    [380] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 380-Insert-Delete-GetRandom-O(1).java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class RandomizedSet {
+
+    ArrayList<Integer> list;
+    HashMap<Integer, Integer> map;
+    Random random;
+
+    public RandomizedSet() {
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        random = new Random();
+    }
+
+    public boolean insert(int val) {
+
+        if (map.containsKey(val)) {
+            return false;
+        }
+
+        list.add(val);
+        map.put(val, list.size() - 1);
+
+        return true;
+    }
+
+    public boolean remove(int val) {
+
+        if (!map.containsKey(val)) {
+            return false;
+        }
+
+        int index = map.get(val);
+        int lastValue = list.get(list.size() - 1);
+
+        list.set(index, lastValue);
+        map.put(lastValue, index);
+
+        list.remove(list.size() - 1);
+        map.remove(val);
+
+        return true;
+    }
+
+    public int getRandom() {
+
+        int index = random.nextInt(list.size());
+
+        return list.get(index);
+    }
+}
